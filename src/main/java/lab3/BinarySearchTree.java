78c935a44f58acd1524700cb2605ab01fe8dd499
Ruslan Zainullin <zayill1998@gmail.com>
2024-03-21 23:33:37 +0300

    LinkedList

--- src/main/java/lab3/BinarySearchTree.java
+++ src/main/java/lab3/BinarySearchTree.java
@@ -31,7 +31,7 @@ public class BinarySearchTree<T> implements Comparator<T> {
 
     @Override
     public int compare(T o1, T o2) {
-        if (o1 > o2)
+        if (o1 == o2)
             return 0;
         else if (o1 > o2) {
             return -1;
